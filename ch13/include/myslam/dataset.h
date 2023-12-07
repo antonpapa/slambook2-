172d2b02732133c2a7c77fc5f80b40c47059e4b5
antonpapa <122083093+antonpapa@users.noreply.github.com>
2023-12-07 10:23:41 +0800

    Update dataset.h

--- ch13/include/myslam/dataset.h
+++ ch13/include/myslam/dataset.h
@@ -15,7 +15,7 @@ class Dataset {
    public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
     typedef std::shared_ptr<Dataset> Ptr;
-    Dataset(const std::string& dataset_path);
+    Dataset(const std::string& dataset_path);//声明成员函数，具体实现看.cpp文件
 
     /// 初始化，返回是否成功
     bool Init();
@@ -24,9 +24,10 @@ class Dataset {
     Frame::Ptr NextFrame();
 
     /// get camera by id
-    Camera::Ptr GetCamera(int camera_id) const {
+    Camera::Ptr GetCamera(int camera_id) const 
+   {
         return cameras_.at(camera_id);
-    }
+   }
 
    private:
     std::string dataset_path_;
